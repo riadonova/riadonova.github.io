bd5add398d44e79f19c238004c73737ff2023135
Vika <victoriaspiridonova95@gmail.com>
2019-04-09 14:47:58 +0700

    js-theory_02

--- js-theory_02/js/index.js
+++ js-theory_02/js/index.js
@@ -1,6 +1,39 @@
-var contentString = '';
-for (var i = 0; i<10; i++) {
-   contentString = contentString + '<div class="box">' + i + '</div>'
-}
+const map = [
+    ['X', 'X', 'X', 'X', 'X'],
+    ['O', 'O', 'X', 'X', 'X'],
+    ['X', 'X', 'O', 'O', 'O'],
+    ['O', 'X', 'X', 'X', 'O'],
+    ['O', 'X', 'X', 'X', 'X'],
+    ['W', 'W', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'X', 'X'],
+    ['X', 'X', 'X', 'W', 'W'],
+    ['X', 'X', 'W', 'W', 'W'],
+    ['X', 'W', 'W', 'X', 'X']
+];
 
-document.getElementById('container').innerHTML = contentString;
+let mapString = '';
+for (let i = 0; i < map.length; i++) {
+    const row = map[i];
+    mapString = mapString + '<div class="row">';
+    for (let j = 0; j < row.length; j++) {
+        const cell = row[j];
+        if (cell === 'X') {
+            mapString = mapString + '<div class="cell land"></div>';
+        } else if (cell === 'W') {
+            mapString = mapString + '<div class ="cell sands"></div>';
+        } else {
+            mapString = mapString + '<div class="cell water"></div>';
+        }
+    }
+
+    mapString = mapString + '</div>';
+}
+document.getElementById('container').innerHTML = mapString;
+
+// let contentString = '';
+// for (let i = 0; i < 10; i++) {
+//     contentString = contentString + '<div class="box">' + i + '</div>'
+// }
+//
+// document.getElementById('container').innerHTML = contentString;
+
